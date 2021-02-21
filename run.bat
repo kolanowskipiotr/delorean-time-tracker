cdd05cb8a5f5eb50698c001fd9715fb1b99f8fc5
pkolanow <piotr.kolanowski@unitygroup.com>
2021-02-21 22:35:51 +0100

    * fix: typoes

--- run.bat
+++ run.bat
@@ -1,5 +1,2 @@
 @ECHO OFF
-start java -jar delorean-time-tracker-0.0.1-SNAPSHOT.jar
-ECHO Waiting [30s] for server start...
-timeout 30
-start http://localhost:9889
+start java -jar delorean-time-tracker-0.0.2-SNAPSHOT.jar
