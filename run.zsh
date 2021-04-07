981b0ce242ce66b476a59619e7cb5f193aa9abc3
pkolanow <piotr.kolanowski@unitygroup.com>
2021-04-07 10:16:07 +0200

    Added script to run with ZSH

---
+++ run.zsh
@@ -0,0 +1,8 @@
+# How to install: Add to ~/.zshrc two below lines
+## DeLorean Time Tracker
+#. {PATH_TO_THIS_FILE}
+
+function dtt {
+	cd /home/pkolanow/personal-workspace/delorean-time-tracker-RELEASES/delorean-time-tracker-1.2.0
+    java -jar delorean-time-tracker-1.2.0.jar
+}
